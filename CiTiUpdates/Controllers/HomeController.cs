ef279babbb8a34637e40200b740f73d88f2a7de5
Ayah <Ayah@DESKTOP-474AN07>
2018-08-13 01:40:36 -1100

    new changes

--- CiTiUpdates/Controllers/HomeController.cs
+++ CiTiUpdates/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace CiTiUpdates.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Admin Login";
 
             return View();
         }
---
+++ CiTiUpdates/Models/Personal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CiTiUpdates.Models
+{
+    public class Personal
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string IdNumber { get; set; }
+        public string CellNumber{ get; set; }
+        public string Email { get; set; }
+        public string StreetAddress { get; set; }
+        public string City{ get; set; }
+        public string State{ get; set; }
+        public string Province{ get; set; }
+        public int Zip { get; set; }
+
+    }
+}
